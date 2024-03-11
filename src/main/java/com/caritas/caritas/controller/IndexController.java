717ab38e4708a220f3db736c3970734cfa605e61
Bautista Guillot <bautistaguillot@hotmail.com>
2024-03-11 16:54:20 -0300

    cargar ultimas 3 noticias en el index

--- src/main/java/com/caritas/caritas/controller/IndexController.java
+++ src/main/java/com/caritas/caritas/controller/IndexController.java
@@ -1,16 +1,43 @@
 package com.caritas.caritas.controller;
 
+import com.caritas.caritas.model.Admin;
+import com.caritas.caritas.model.Publicacion;
+import com.caritas.caritas.service.AdminService;
+import com.caritas.caritas.service.PublicacionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
-        @GetMapping("/")
-        public String index() {
-            return "index";
+        @Autowired
+        private PublicacionService publicacionService;
+
+        @Autowired
+        private AdminService adminService;
+
+    @GetMapping("/")
+    public String mostrarListado(Model model) {
+        List<Admin> admins = adminService.getAllAdmins();
+        model.addAttribute("admin", admins);
+
+        // Obtener las últimas 3 publicaciones
+        List<Publicacion> publicaciones = publicacionService.getAll();
+        int totalPublicaciones = publicaciones.size();
+        int indiceInicio = totalPublicaciones - 3;
+        if (indiceInicio < 0) {
+            indiceInicio = 0;
         }
+        List<Publicacion> ultimasPublicaciones = publicaciones.subList(indiceInicio, totalPublicaciones);
+
+        model.addAttribute("publicaciones", ultimasPublicaciones);
+        return "index";
+    }
 
         @GetMapping("/quienes-somos")
         public String quienesSomos() {
@@ -36,6 +63,8 @@ public class IndexController {
         public String donar() {
             return "donar";
         }
+
+
 
 
 
