816050132e76522e49018b56dde24974012a3934
Bautista Guillot <bautistaguillot@hotmail.com>
2024-03-13 16:47:51 -0300

    borre las cartas que no iban.

--- src/main/java/com/caritas/caritas/controller/IndexController.java
+++ src/main/java/com/caritas/caritas/controller/IndexController.java
@@ -44,21 +44,6 @@ public class IndexController {
             return "quienes-somos";
         }
 
-        @GetMapping("/info_carta_1")
-        public String infoCarta1() {
-        return "info_carta_1";
-    }
-
-        @GetMapping("/info_carta_2")
-        public String infoCarta2() {
-            return "info_carta_2";
-        }
-
-        @GetMapping("/info_carta_3")
-        public String infoCarta3() {
-            return "info_carta_3";
-        }
-
         @GetMapping("/donar")
         public String donar() {
             return "donar";
